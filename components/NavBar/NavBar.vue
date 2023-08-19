094ce2088f7b82cdd52db52a2c843ac57ed54c00
Michael Nef <michael@nef.net.au>
2023-08-19 22:32:53 +0800

    navbar redesign

--- components/NavBar/NavBar.vue
+++ components/NavBar/NavBar.vue
@@ -1,12 +1,24 @@
 <template>
-  <nav class="h-[64px] flex flex-row">
-    <NuxtLink class="flex items-center justify-center w-1/2 text-white bg-primary" to="/">
-        <div>Map</div>
+  <nav class="h-[64px] flex flex-row bg-gray-100 shadow-lg p-2 rounded-md">
+    <NuxtLink class="flex items-center justify-center w-1/2 transition-transform transform hover:scale-105 rounded-md" to="/">
+        <div class="flex flex-col items-center">
+            <span class="material-icons text-primary mb-1">map</span>
+            <div class="text-sm text-primary">Map</div>
+        </div>
     </NuxtLink>
-    <NuxtLink class="flex items-center justify-center w-1/2 text-white bg-[#F27921] text-center" to="/chat">
-        <div>Preparation Plan</div>
+    <NuxtLink class="flex items-center justify-center w-1/2 transition-transform transform hover:scale-105 rounded-md" to="/chat">
+        <div class="flex flex-col items-center">
+            <span class="material-icons text-[#F27921] mb-1">chat_bubble</span>
+            <div class="text-sm text-[#F27921]">Preparation Plan</div>
+        </div>
     </NuxtLink>
   </nav>
 </template>
 
 <script setup lang="ts"></script>
+
+<style scoped>
+.material-icons {
+  font-size: 24px;
+}
+</style>
--- components/NavBar/TopBar.vue
+++ components/NavBar/TopBar.vue
@@ -1,5 +1,70 @@
 <template>
-    <div class="h-[56px] bg-secondary flex items-center justify-center px-2">
-        <img src="assets/images/logo.png" class="h-[80%] object-contain"/>
-    </div>
+  <nav class="navbar">
+    <h1 class="navbar-header">WeatherWise</h1>
+  </nav>
 </template>
+
+<script setup lang="ts">
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+
+const navLinks = [
+    { name: 'Map', path: '/' },
+    { name: 'Preparation Plan', path: '/chat' }
+];
+
+const currentPath = ref(window.location.pathname);
+
+const updatePath = () => {
+  currentPath.value = window.location.pathname;
+};
+
+onMounted(() => {
+  window.addEventListener('popstate', updatePath);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener('popstate', updatePath);
+});
+</script>
+
+<style scoped>
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem 2rem;
+  background-color: #5CC89B;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+}
+
+.navbar-header {
+  color: white;
+  font-size: 2.5rem;      /* Bigger font size */
+  font-weight: bold;      /* Bold text for emphasis */
+  letter-spacing: 1.5px; /* Optional: Slight spacing between letters for better readability */
+  text-shadow: 2px 2px 8px rgba(0, 0, 0, 0.2); /* Optional: Text shadow for depth */
+  margin-bottom: 1.5rem; /* More margin to give it room to breathe */
+}
+
+.nav-item {
+  margin-right: 1rem;
+}
+
+.nav-link {
+  display: block;
+  padding: 0.5rem 1rem;
+  color: white;
+  text-decoration: none;
+  transition: background-color 0.3s, transform 0.2s;
+  border-radius: 5px;
+  font-weight: 500;
+}
+
+.nav-link:hover {
+  background-color: rgba(255, 255, 255, 0.1);
+  transform: scale(1.05);
+}
+
+.nav-link.active {
+  background-color: #F27921;
+}
+</style>
